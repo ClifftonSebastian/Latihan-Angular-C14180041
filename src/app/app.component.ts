84162859af9972341b84b0fc25842df7f7100961
ClifftonSebastian <c14180041@john.petra.ac.id>
2020-04-15 10:13:29 +0700

    Final Commit (HASIL)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
+  name = 'Cliffton';
+  inputa=0;
+  inputb=0;
+  hasil=0;
+  output=[];
+  outputPola(input, a, b){
+    this.output = [];
+    let max =0;
+    if(a>b){
+      max=a;
+    }
+    else{
+      max=b;
+    }
+    if(input % 2 == 0){
+      //genap
+      for(let i =max;i<=input;i++){
+        let str="";
+        for(let j=0;j<i;j++){
+          str+="*";
+        }
+        this.output.push(str);
+      }
+    }
+    else{
+      //ganjil
+      let counta=b;
+      let countb=1;
+      for(let i =0;i<max;i++){
+        let str="";
+        for(let j=0;j<counta;j++){
+          str+="*";
+        }
+        if(countb<=a){
+          for(let k=0;k<countb;k++){
+            str+="o";
+          }
+        }
+        counta--;
+        countb++;
+        this.output.push(str);
+      }
+    }
+  }
+  Hitung(a,b){
+    this.inputa = parseInt(a);
+    this.inputb = parseInt(b);
+    this.hasil = Math.pow(this.inputa,this.inputb);
+    this.outputPola(this.hasil, this.inputa, this.inputb);
+  }
+
 }
